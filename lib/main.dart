a4c65031e03e3d884eec9e6fc11f8a50d1584daf
abhi <abhi1124365@gmail.com>
2023-11-20 17:27:19 +0530

    ios set up

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'Routes/Routes.dart';
 import 'package:get/get.dart';
 import 'Routes/RoutesString.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';    
 import 'package:get_storage/get_storage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:steamhouse/config/LocalString.dart';
